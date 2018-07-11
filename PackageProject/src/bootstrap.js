be149af05be77f9cea54eda2efc3528c5c430351
Code <Code@batc.local>
2018-07-11 08:41:56 -0600

    changed port and app title

---
+++ PackageProject/src/bootstrap.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+
+const twoWeeksFormNow = moment().add(14, 'days');
+
+console.log(twoWeeksFormNow);
---
+++ notes.js
@@ -0,0 +1,11 @@
+// DOM -> Document Object Model
+
+const user = {
+    name: 'Tyler',
+    email: 'tylernapoli01@gmail.com',
+    favorites: [
+        'Chick-Fil-A',
+        'Taco Bell',
+    ]
+}
+
