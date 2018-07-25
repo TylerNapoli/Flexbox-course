5547810c07846949a61239dd2d50ea8a5fef90e0
Code <Code@batc.local>
2018-07-25 08:28:56 -0600

    fixed grid and added arrow component

---
+++ TN_Schedule-master/src/components/library/libraryCourse.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Icon from '../icons';
+import Arrow from '../arrow';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <div className="library-course__title-arrow">
+                    <label className="library-course__title">Problem Solving</label>
+                    {Icon('fas fa-check', 'library-course__icon')}
+                </div>
+                <Arrow className="library-course__arrow" />
+                {/* action button component */}
+                <div className="library-course__discription">
+                    <label>Course Discription</label>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque scelerisque augue ut est tempor bibendum. Donec at diam auctor, vehicula leo sit amet, placerat nibh. Vestibulum sit amet erat non ex sollicitudin volutpat id blandit odio.</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LibraryCourse;
